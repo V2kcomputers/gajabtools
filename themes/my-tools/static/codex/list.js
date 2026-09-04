363b5b2647c7611f0c2f7687125ac48e9858774f
Vishal Singh <tvishalllodhi@gmail.com>
2026-09-04 13:23:24 +0530

    update post

---
+++ themes/my-tools/static/codex/list.js
@@ -0,0 +1,137 @@
+const links = [
+    {
+        id: "3d-text-Generator-pro",
+        title: "3D Text Prompt Generator Pro",
+        icon: "🖼️",
+        url: `${_q7Xk}Promt-Manage-System/ai-3d-glow-text-promt-generator${_m8Vr}`
+    },
+    {
+        id: "doc-list-prompt",
+        title: "AI Image Bulleton (Docs list) Prompt Pro Maker",
+        icon: "↕️",
+        url: `${_q7Xk}Promt-Manage-System/document-list-and-side-by-side-items${_m8Vr}`
+    },
+    {
+        id: "ai-image-prompt",
+        title: "AI Human PNG Prompt Generator",
+        icon: "🧑‍💼",
+        url: `${_q7Xk}Promt-Manage-System/AI-Human-PNG-Prompt-Generator${_m8Vr}`
+    },
+    {
+        id: "jobs-banner-prompt",
+        title: "Jobs/Recruitment Baner Prompt Maker",
+        icon: "↔️",
+        url: `${_q7Xk}Promt-Manage-System/jobs-recruitment-jobs-baner-promt${_m8Vr}`
+    },
+    {
+        id: "ai-baner-prompt",
+        title: "AI Baner Generator Promts",
+        icon: "🎟️",
+        url: `${_q7Xk}Promt-Manage-System/ai-baner-Image-Prompt-Generator${_m8Vr}`
+    },
+    {
+        id: "baner-image-extractor",
+        title: "Banner Content Extractor Promts",
+        icon: "🖼️",
+        url: `${_q7Xk}Promt-Manage-System/Poster%20Extract%20promt${_m8Vr}`
+    },
+    {
+        id: "multi-links",
+        title: "Multi Link in html table/toml",
+        icon: "🖇️",
+        url: `${_q7Xk}post-creator/pro/multi-link-toml-generator-pro${_m8Vr}`
+    },
+    {
+        id: "date-and-permalink",
+        title: "Date/Lastmode and Permalink",
+        icon: "🗓️",
+        url: `${_q7Xk}gt-html/ifreme/permalink-or-date-and-lastmod${_m8Vr}`
+    },
+    {
+        id: "add-links",
+        title: "+ Add Useful Links",
+        icon: "🔗",
+        url: `${_q7Xk}gt-html/ifreme/add-useful-links${_m8Vr}`
+    },
+    {
+        id: "google-trusted-link",
+        title: "Google Redirect Link Generator",
+        icon: "🔗",
+        url: `${_q7Xk}gt-html/google-trusted-redirect-link-maker${_m8Vr}`
+    }
+];
+
+const rightLinks = [
+    {
+        id: "css-minifier",
+        title: "CSS Minifier and Styler",
+        icon: "🧮",
+        url: `${_q7Xk}htmlstyles-tools/CSS-Minifier${_m8Vr}`
+    },
+    {
+        id: "html-minifier",
+        title: "HTML Minifier and Styler",
+        icon: "🧱",
+        url: `${_q7Xk}htmlstyles-tools/HTML%20Minifier${_m8Vr}`
+    },
+    {
+        id: "js-obfuscator",
+        title: "JS Obfuscator",
+        icon: "🎗",
+        url: `${_q7Xk}htmlstyles-tools/professional-js-obfuscator${_m8Vr}`
+    },
+    {
+        id: "3d-text-Generator-pro",
+        title: "3D Text Prompt Generator Pro",
+        icon: "🖼️",
+        url: `${_q7Xk}Promt-Manage-System/ai-3d-text-promt-generator${_m8Vr}`
+    },
+    {
+        id: "text-escape-formatter",
+        title: "Text Escape Formater",
+        icon: "🎫",
+        url: `${_q7Xk}gt-html/text-escape-formatter${_m8Vr}`
+    },
+    {
+        id: "doc-list-prompt",
+        title: "AI Image Bulleton (Docs list) Prompt Pro Maker",
+        icon: "↕️",
+        url: `${_q7Xk}Promt-Manage-System/document-list-generator-ultra-pro${_m8Vr}`
+    },
+    {
+        id: "side-list-prompt",
+        title: "Side-by-Side Bulleton (list) Pro Prompt Maker",
+        icon: "↔️",
+        url: `${_q7Xk}Promt-Manage-System/side-by-side-list-with-icon-ultra-pro${_m8Vr}`
+    },
+    {
+        id: "search-and-contact",
+        title: "Search and Contect Deshborad",
+        icon: "🔎",
+        url: `${_q7Xk}gt-html/ifreme/websearch-and-content-google-sheets${_m8Vr}`
+    },
+    {
+        id: "daily-hisab",
+        title: "Daily Hisab Entry Form",
+        icon: "📝",
+        url: `${_q7Xk}balence-sheet/balence-calcutation-daily${_m8Vr}`
+    },
+    {
+        id: "balence-dashboard",
+        title: "Daily Balence Dashboard",
+        icon: "📊",
+        url: `${_q7Xk}balence-sheet/balence-dashboard${_m8Vr}`
+    },
+    {
+        id: "html-viewer",
+        title: "HTML Viewer and Editor",
+        icon: "👁️",
+        url: `${_q7Xk}htmlstyles-tools/HTML-Viewer-and-Editor${_m8Vr}`
+    },
+    {
+        id: "yp-post-update",
+        title: "YP Post Update Dashboard",
+        icon: "📝",
+        url: `${_q7Xk}gt-html/iframe/yp-post-update-dashboard-pro${_m8Vr}`
+    }
+];
